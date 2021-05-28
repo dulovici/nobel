2dfa395e8789a960c72ef63ee09c265931b9e3c8
dulovici <igorx24@gmail.com>
2021-05-28 20:28:22 +0200

    nobelovci

---
+++ script.js
@@ -0,0 +1,83 @@
+'use strict'
+
+
+var characters = [
+    {
+        fullName: 'Albert Einstein',
+        priceFor: 'Nobel Prize in Physics-1921',
+        photo: './imgs/albert_einstein.jpeg'
+    },
+    {
+        fullName: 'Alexander Fleming',
+        priceFor: 'Nobel Prize in Physiology-1945',
+        photo: './imgs/alexander_fleming.jpeg'
+    },
+    {
+        fullName: 'Bob Dylan',
+        priceFor: 'Nobel Prize in Literature-2016 ',
+        photo: './imgs/bob_dylan.jpeg'
+    },
+    {
+        fullName: 'Mikhail Gorbachev',
+        priceFor: 'Nobel Prize for Peace-1990',
+        photo: './imgs/Gorbachev_Mikhail.jpeg'
+    },
+    {
+        fullName: 'Henry Kissinger',
+        priceFor: 'Nobel Prize for Peace-1973',
+        photo: './imgs/henry_kissinger.jpeg'
+    },
+    {
+        fullName: 'Herman Muller',
+        priceFor: 'Nobel Prize in Physiology-1946',
+        photo: './imgs/herman_muller.jpeg'
+    },
+    {
+        fullName: 'Ivo Andric',
+        priceFor: 'Nobel Prize in Literature-1961',
+        photo: './imgs/ivo_andric.jpg'
+    },
+    {
+        fullName: 'Jean Paul Gisele',
+        priceFor: 'The Nobel Prize in Literature-1964',
+        photo: './imgs/Jean-Paul-Sartre-Gisele.jpeg'
+    },
+    {
+        fullName: 'Kofi Annan',
+        priceFor: 'Nobel Prize for Peace-2001',
+        photo: './imgs/Kofi_Annan.jpeg'
+    },
+    {
+        fullName: 'Marie Curie',
+        priceFor: 'Nobel Prize for Physics-1903',
+        photo: './imgs/marie_curie.jpeg'
+    },
+    {
+        fullName: 'Martin Luter King',
+        priceFor: 'Nobel Prize for Peace-1964',
+        photo: './imgs/Martin_Luther_King.jpeg'
+    },
+    {
+        fullName: 'Mother Teresa',
+        priceFor: 'Nobel Prize for Peace-1979',
+        photo: './imgs/mother-teresa.jpeg'
+    },
+    {
+        fullName: 'Nelson Mendela',
+        priceFor: 'Nobel Prize for Peace-1993',
+        photo: './imgs/Nelson_Mandela.jpeg'
+    },
+    {
+        fullName: 'Theodore Rosevelt',
+        priceFor: 'Nobel Prize for Peace-1906',
+        photo: './imgs/theo_rosevelt.jpeg'
+    },
+    {
+        fullName: 'Werner Heisenberg',
+        priceFor: 'Nobel Prize in Physics-1932',
+        photo: './imgs/werner_heisenberg.jpeg'
+    },
+
+]
+
+console.log(characters);
